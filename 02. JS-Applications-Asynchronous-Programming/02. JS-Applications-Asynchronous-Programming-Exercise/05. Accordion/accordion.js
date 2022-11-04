703d6c434dd3e7c0b48e1f42047a4cdf0b88397c
NedDinev <ned.dinev@gmail.com>
2022-11-04 14:16:47 +0200

    New tasks completed

--- 02. JS-Applications-Asynchronous-Programming/02. JS-Applications-Asynchronous-Programming-Exercise/05. Accordion/accordion.js
+++ 02. JS-Applications-Asynchronous-Programming/02. JS-Applications-Asynchronous-Programming-Exercise/05. Accordion/accordion.js
@@ -1,3 +1,41 @@
-function solution() {
-    //TODO .....
+async function solution() {
+  let response = await fetch(
+    "http://localhost:3030/jsonstore/advanced/articles/list"
+  );
+  let data = await response.json();
+  let main = document.getElementById("main");
+  Object.values(data).forEach(async ({ _id, title }) => {
+    let detailsResponse = await fetch(
+      `http://localhost:3030/jsonstore/advanced/articles/details/${_id}`
+    );
+    let detailsData = await detailsResponse.json();
+    let newAccordion = document.createElement("div");
+    newAccordion.innerHTML = `
+    <div class="accordion">
+            <div class="head">
+                <span>${title}</span>
+                <button class="button" id="${_id}">More</button>
+            </div>
+            <div class="extra">
+                <p>${detailsData.content}</p>
+            </div>
+    </div>
+    `;
+    main.appendChild(newAccordion);
+    let currentBtn = document.getElementById(`${_id}`);
+    currentBtn.addEventListener("click", expand);
+  });
+  function expand(event) {
+    let btn = event.target;
+    let accordion = btn.parentElement.parentElement;
+    let extraContent = accordion.querySelector(".extra");
+
+    if (btn.textContent == "More") {
+      btn.textContent = "Less";
+      extraContent.style.display = "block";
+    } else {
+      btn.textContent = "More";
+      extraContent.style.display = "none";
+    }
+  }
 }
--- 02. JS-Applications-Asynchronous-Programming/02. JS-Applications-Asynchronous-Programming-Exercise/06.Blog/app.js
+++ 02. JS-Applications-Asynchronous-Programming/02. JS-Applications-Asynchronous-Programming-Exercise/06.Blog/app.js
@@ -1,5 +1,54 @@
 function attachEvents() {
-    console.log('TODO...');
+  const btnLoadPosts = document.getElementById("btnLoadPosts");
+  const posts = document.getElementById("posts");
+  const btnViewPost = document.getElementById("btnViewPost");
+
+  btnLoadPosts.addEventListener("click", loadPosts);
+  btnViewPost.addEventListener("click", viewPosts);
+
+  async function loadPosts() {
+    posts.innerHTML = "";
+    const postsResponse = await fetch(
+      `http://localhost:3030/jsonstore/blog/posts`
+    );
+    const postsData = await postsResponse.json();
+    Object.entries(postsData).forEach(([id, obj]) => {
+      const newOption = document.createElement("option");
+      newOption.value = obj.id;
+      newOption.textContent = obj.title;
+      posts.appendChild(newOption);
+    });
+  }
+  async function viewPosts() {
+    const postResponse = await fetch(
+      `http://localhost:3030/jsonstore/blog/posts/${posts.value}`
+    );
+    const postData = await postResponse.json();
+    const postTitle = document.getElementById("post-title");
+
+    if (posts.value != "") {
+      postTitle.textContent = postData.title;
+    }
+
+    const postBody = document.getElementById("post-body");
+    postBody.textContent = postData.body;
+
+    const commentsResponse = await fetch(
+      `http://localhost:3030/jsonstore/blog/comments`
+    );
+    const commentsData = await commentsResponse.json();
+    const postComments = document.getElementById("post-comments");
+    postComments.innerHTML = "";
+
+    for (const [key, value] of Object.entries(commentsData)) {
+      if (value.postId == posts.value) {
+        const newLi = document.createElement("li");
+        newLi.id = value.id;
+        newLi.textContent = value.text;
+        postComments.appendChild(newLi);
+      }
+    }
+  }
 }
 
 attachEvents();
