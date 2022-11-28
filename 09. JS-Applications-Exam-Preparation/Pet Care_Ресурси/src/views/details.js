5b089ff8d03f9f9675a9f3577d278e0bde0137e4
NedDinev <ned.dinev@gmail.com>
2022-11-28 22:00:46 +0200

    .

---
+++ 09. JS-Applications-Exam-Preparation/Pet Care_Ресурси/src/views/details.js
@@ -0,0 +1,94 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import {
+  deletePet,
+  donationFromUser,
+  getPetsById,
+  petDonation,
+  totalPetDonations,
+} from "../api/pets.js";
+import { getUserData } from "../api/utility.js";
+
+const detailsTemplate = (
+  pet,
+  isOwner,
+  onDelete,
+  donationsCount
+) => html` <section id="detailsPage">
+  <div class="details">
+    <div class="animalPic">
+      <img src=${pet.image} />
+    </div>
+    <div>
+      <div class="animalInfo">
+        <h1>Name: ${pet.name}</h1>
+        <h3>Breed: ${pet.breed}</h3>
+        <h4>Age: ${pet.age}</h4>
+        <h4>Weight: ${pet.weight}</h4>
+        <h4 class="donation">Donation: $</h4>
+      </div>
+      <div class="actionBtn">
+        ${isOwner
+          ? html`<a href="/edit/${pet._id}" class="edit">Edit</a>
+              <a href="javascript:void(0)" @click=${onDelete} class="remove"
+                >Delete</a
+              > `
+          : ""}
+      </div>
+    </div>
+  </div>
+</section>`;
+
+export async function detailsPage(ctx) {
+  const pet = await getPetsById(ctx.params.id);
+  const user = getUserData();
+  const isOwner = user?._id === pet._ownerId;
+  const isLogged = user !== undefined;
+  let donationsCount;
+  let userDonated;
+
+  if (user) {
+    userDonated = await donationFromUser(ctx.params.id, ctx.params._id);
+  }
+  donationsCount = await totalPetDonations(ctx.params.id);
+  ctx.render(
+    detailsTemplate(
+      pet,
+      isOwner,
+      onDelete,
+      isLogged,
+      donationsCount,
+      onClickDonate,
+      userDonated
+    )
+  );
+
+  async function onClickDonate(e) {
+    const donate = {
+      pet,
+    };
+    e.target.style.display = "none";
+    await petDonation(donate);
+  }
+  donationsCount = await totalPetDonations(ctx.params.id);
+  userDonated = await donationFromUser(ctx.params.id, ctx.params._id);
+  ctx.render(
+    detailsTemplate(
+      pet,
+      isOwner,
+      onDelete,
+      isLogged,
+      donationsCount,
+      onClickDonate,
+      userDonated
+    )
+  );
+
+  async function onDelete() {
+    const dialog = confirm("Are you sure you want to delete this article?");
+
+    if (dialog) {
+      await deletePet(ctx.params.id);
+      ctx.page.redirect("/");
+    }
+  }
+}
---
+++ 09. JS-Applications-Exam-Preparation/Pet Care_Ресурси/src/views/register.js
@@ -0,0 +1,65 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { register } from "../api/users.js";
+import { updateNav } from "../api/utility.js";
+
+const registerTemplate = (onSubmit) => html` <section id="registerPage">
+  <form class="registerForm" @submit=${onSubmit}>
+    <img src="./images/logo.png" alt="logo" />
+    <h2>Register</h2>
+    <div class="on-dark">
+      <label for="email">Email:</label>
+      <input
+        id="email"
+        name="email"
+        type="text"
+        placeholder="steven@abv.bg"
+        value=""
+      />
+    </div>
+    <div class="on-dark">
+      <label for="password">Password:</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="********"
+        value=""
+      />
+    </div>
+    <div class="on-dark">
+      <label for="repeatPassword">Repeat Password:</label>
+      <input
+        id="repeatPassword"
+        name="repeatPassword"
+        type="password"
+        placeholder="********"
+        value=""
+      />
+    </div>
+    <button class="btn" type="submit">Register</button>
+    <p class="field">
+      <span>If you have profile click <a href="/login">here</a></span>
+    </p>
+  </form>
+</section>`;
+
+export function registerPage(ctx) {
+  ctx.render(registerTemplate(onSubmit));
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const repassword = formData.get("repeatPassword");
+    if (!email || !password || !repassword) {
+      return alert("All fields are required!");
+    }
+    if (password !== repassword) {
+      return alert("Passwords don't match");
+    }
+    await register(email, password);
+    updateNav();
+    ctx.page.redirect("/");
+  }
+}
